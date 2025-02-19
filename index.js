21b57d573f588b168fecf428330e2cb73265fb00
Jennifer MERAT <131549759+refinejcode49@users.noreply.github.com>
2025-02-19 17:48:55 +0100

    Solved lab

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const stringConcatenated = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4;
 
 // Print out the concatenated string
-
+console.log(stringConcatenated);
 
 
 
@@ -22,10 +22,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+const lastPart1 = part1.slice(0, 3) + part1.slice(-1).toUpperCase();
+const lastPart2 = part2.slice(0, 5) + part2.slice(-1).toUpperCase();
 // Print the cameLtaiL-formatted string
-
+console.log(lastPart1 + lastPart2);
 
 
 
@@ -35,10 +35,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const billWithTax = billTotal + (billTotal * (15 / 100));
 
 // Print out the tipAmount
-
+console.log(billWithTax);
 
 
 
@@ -47,6 +47,7 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
+//Math.floor(Math.random() * (max - min + 1)) + min;
 
 
 // Print the generated random number
@@ -61,16 +62,16 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // => false
 
-const expression2 = a || b;
+const expression2 = a || b; // => true
 
-const expression3 = !a && b;
+const expression3 = !a && b; // NOTtrue AND false => false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); // NOT(false) => true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; // NOTtrue OR NOTfalse false OR true => true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // NOT(true or false) => false
 
-const expression7 = a && a;
+const expression7 = a && a; // => true
